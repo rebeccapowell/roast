8307804150f8ef5b9a5fb0384c54b3e996946f00
Daniel Doberenz <daniel.doberenz@lichtblick.de>
2025-10-09 15:27:19 +0200

    Changed to successful deployment

--- src/CoffeeTalk.Web/build-and-push-docker.sh
+++ src/CoffeeTalk.Web/build-and-push-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t coffeetalk-web:latest -t lbhackweekroast2025.azurecr.io/coffeetalk-web:latest .
+docker build -t coffeetalk-web:latest -t lbhackweekroast2025.azurecr.io/coffeetalk-web:latest --platform linux/amd64 .
 docker push lbhackweekroast2025.azurecr.io/coffeetalk-web:latest
